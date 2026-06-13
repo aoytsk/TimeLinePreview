d747876b39e5c1b6fcf7501f7e2c71063d913073
aoya <aoyatk@gmail.com>
2026-06-13 20:23:43 +0900

    ui: add settings dialog resource

---
+++ TimeLinePreview/resource.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define IDD_SETTINGS_DIALOG             101
+
+#define IDC_THUMB_W                     1001
+#define IDC_TIMER_MS                    1002
+#define IDC_SHOW_THUMB                  1003
+#define IDC_SHOW_HEAD                   1004
+#define IDC_SHOW_PARAM                  1005
+#define IDC_SHOW_FILTERS                1006
+#define IDC_SHOW_FILTER_PARAMS          1007
+#define IDC_LABEL_THUMB_W               1008
+#define IDC_LABEL_TIMER_MS              1009
